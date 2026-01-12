10e4a8d794f94f5b3d8f93bbbfd2e1ea7c70282e
Mykhailo Mushynskyi <45673852+Krakenus00@users.noreply.github.com>
2026-01-13 00:44:30 +0100

    Add XCode

---
+++ src/KrakenSwiftUI/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  KrakenRenderer
+//
+//  Created by Mykhailo Mushynskyi on 13/01/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
